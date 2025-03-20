62de9052dc60862bdce4a2d3499a663958ea6d9a
echeadle <echeadle@gmail.com>
2025-03-20 10:47:21 -0600

    fixed code path in import

--- src/duplicate_finder.py
+++ src/duplicate_finder.py
@@ -1,7 +1,7 @@
 import os
 import hashlib
 import logging
-from db_utils.db_utils import create_db, store_hash_in_db
+from src.db_utils.db_utils import create_db, store_hash_in_db
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
